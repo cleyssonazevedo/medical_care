230446eb4de177bb1662651aede77d7dedad9551
Cleysson Azevedo <cleyssonazevedo@outlook.com>
2018-04-17 10:18:07 -0300

    Adição de novos itens

--- script.sql
+++ script.sql
@@ -1,17 +1,9 @@
-CREATE DATABASE medical;
+DROP DATABASE IF EXISTS medical;
+CREATE DATABASE IF NOT EXISTS medical;
 
 USE medical;
 
-CREATE TABLE paciente (
-    ident           INT PRIMARY KEY AUTO_INCREMENT,
-    nome            VARCHAR(255) NOT NULL,
-    naturalidade    VARCHAR (100) NOT NULL,
-    sexo            VARCHAR(50) NOT NULL,
-    cpf             CHAR(15),
-    id_endereco     INT NOT NULL UNIQUE
-);
-
-CREATE TABLE endereco (
+CREATE TABLE IF NOT EXISTS endereco (
     id          INT PRIMARY KEY AUTO_INCREMENT,
     logradouro  VARCHAR(100) NOT NULL,
     numero      VARCHAR(10),
@@ -21,16 +13,51 @@ CREATE TABLE endereco (
     cep         CHAR(9) NOT NULL
 );
 
-ALTER TABLE paciente ADD CONSTRAINT fk_endereco FOREIGN KEY (id_endereco) REFERENCES endereco(id);
+CREATE TABLE IF NOT EXISTS paciente (
+    ident           INT PRIMARY KEY AUTO_INCREMENT,
+    nome            VARCHAR(255) NOT NULL,
+    naturalidade    VARCHAR (100) NOT NULL,
+    sexo            VARCHAR(50) NOT NULL,
+    cpf             CHAR(15),
+    id_endereco     INT NOT NULL UNIQUE,
+    FOREIGN KEY (id_endereco) REFERENCES endereco(id)
+);
 
 INSERT INTO endereco (logradouro, numero, bairro, cidade, estado, cep) VALUES
- ('Rua 1', '123', 'Vila 10', 'São Paulo', 'SP', '08552-122'),
- ('Rua 2', '123', 'Vila 20', 'São Paulo', 'SP', '08552-123'),
- ('Rua 3', '123', 'Vila 30', 'São Paulo', 'SP', '08552-124'),
- ('Rua 4', '123', 'Vila 40', 'São Paulo', 'SP', '08552-125');
+('Rua 1', '123', 'Vila 10', 'São Paulo', 'SP', '08552-122'),
+('Rua 2', '123', 'Vila 20', 'São Paulo', 'SP', '08552-123'),
+('Rua 3', '123', 'Vila 30', 'São Paulo', 'SP', '08552-124'),
+('Rua 4', '123', 'Vila 40', 'São Paulo', 'SP', '08552-125');
 
 INSERT INTO paciente (nome, naturalidade, sexo, cpf, id_endereco) VALUES
 ('José', 'Brasileiro', 'Masculino', '123.123.132-12', 1),
 ('Maria', 'Brasileiro', 'Feminino', '132.123.132-12', 2),
 ('Stefanny', 'Brasileiro', 'Outro', '321.123.132-12', 3),
 ('Rogério', 'Italiano', 'Não Informado', '123.123.132-14', 4);
+
+
+DELIMITER $$
+CREATE PROCEDURE sp_inserirPaciente 
+(
+    nome            VARCHAR(255),
+    naturalidade    VARCHAR (100),
+    sexo            VARCHAR(50),
+    cpf             CHAR(15),
+    logradouro  	VARCHAR(100),
+    numero      	VARCHAR(10),
+    bairro      	VARCHAR(100),
+    cidade      	VARCHAR(255),
+    estado      	CHAR(2),
+    cep         	CHAR(9)
+)
+BEGIN
+	DECLARE EXIT HANDLER FOR SQLEXCEPTION ROLLBACK;
+	START TRANSACTION;
+		INSERT INTO endereco (logradouro, numero, bairro, cidade, estado, cep) VALUES
+		(logradouro, numero, bairro, cidade, estado, cep);
+	
+		INSERT INTO paciente (nome, naturalidade, sexo, cpf, id_endereco) VALUES
+			(nome, naturalidade, sexo, cpf, LAST_INSERT_ID());
+    COMMIT;
+END $$
+DELIMITER ;
--- src/com/service/PacienteService.java
+++ src/com/service/PacienteService.java
@@ -19,7 +19,7 @@ import com.model.Paciente;
 public class PacienteService {
 
     final String DRIVER = "com.mysql.jdbc.Driver";
-    final String URL = "jdbc:mysql://localhost:3306/bd_medical_care?autoReconnect=true&useSSL=false";
+    final String URL = "jdbc:mysql://localhost:3306/medical?autoReconnect=true&useSSL=false";
 
     public PacienteService() {
 
@@ -37,7 +37,7 @@ public class PacienteService {
             PreparedStatement statement = con.prepareStatement(sql);
             ResultSet result = statement.executeQuery();
 
-            List<Paciente> pacientes = new ArrayList<>();
+            List<Paciente> pacientes = new ArrayList<Paciente>();
             while (result.next()) {
                 Paciente paciente = new Paciente.Builder()
                         .setId(result.getLong("id"))
@@ -55,53 +55,13 @@ public class PacienteService {
             
             return pacientes;
         } catch (Exception e) {
+        	e.printStackTrace();
             JOptionPane.showMessageDialog(null, "Falha ao tentar conectar com o Banco", "Falha", JOptionPane.ERROR_MESSAGE);
             return null;
         }
     }
 
     public void save(Identificacao iden) throws Exception {
-        try {
-            Connection con = this.getConnection();
-            String sql = "INSERT INTO endereco(logradouro, numero, bairro, cidade, estado, cep) VALUES (?, ?, ?, ?, ?)";
-            PreparedStatement statement = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-            statement.setString(0, iden.getEndereco().getLogradouro());
-            statement.setString(1, iden.getEndereco().getNumero());
-            statement.setString(2, iden.getEndereco().getBairro());
-            statement.setString(3, iden.getEndereco().getCidade());
-            statement.setString(4, iden.getEndereco().getEstado());
-            statement.setString(5, iden.getEndereco().getCep());
-            
-            int affect = statement.executeUpdate();
-            if (affect == 0) {
-                throw new Exception("Falha ao gravar dados");
-            }
-            
-            try(ResultSet keys = statement.getGeneratedKeys()) {
-                if(keys.next()) {
-                    Long id = keys.getLong(1);
-                    try {
-                        sql = "INSERT INTO paciente (nome, naturalidade, sexo, cpf, id_endereco) VALUES (?, ?, ?, ?, ?)";
-                        statement = con.prepareStatement(sql);
-                        statement.setString(0, iden.getNome());
-                        statement.setString(1, iden.getNaturalidade());
-                        statement.setString(2, iden.getSexo());
-                        statement.setString(3, iden.getCpf());
-                        statement.setLong(4, id);
-
-                        ResultSet result = statement.executeQuery();
-                        
-                        result.close();
-                        statement.close();
-                        con.close();
-                        JOptionPane.showMessageDialog(null, "Dados Inseridos", "Sucesso", JOptionPane.INFORMATION_MESSAGE);
-                    } catch (Exception e) {
-                        throw  e;
-                    }
-                }
-            }
-        } catch (Exception e) {
-            throw e;
-        }
+       
     }
 }
